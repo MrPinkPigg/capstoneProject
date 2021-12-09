bf4eec9b99d8167f9482e557dde73428c8cff63a
KevinKoudelka <skkovi14@gmail.com>
2021-12-09 18:22:17 -0500

    added different table sizes

--- plate_builder/src/App.js
+++ plate_builder/src/App.js
@@ -5,8 +5,8 @@ import Plate from './components/Plate';
 import Barcodes from './components/Barcodes';
 import logo from './logo.svg';
 import './App.css';
-var row;
-var col;
+var row = 12;
+var col = 8;
 class App extends Component{
   render() {
     return (
@@ -15,18 +15,38 @@ class App extends Component{
           <h2>PlateBuilder</h2>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <ul className="navbar-nav mr-auto">
-            <li><Link to={'/plate'} className="nav-link"> Plate </Link></li>
+            <li><Link to={'/plate'} className="nav-link"> 96 Plate </Link></li>
+            <li><Link to={'/plate384'} className="nav-link"> 384 Plate </Link></li>
+            <li><Link to={'/plate1536'} className="nav-link"> 1536 Plate </Link></li>
             <li><Link to={'/barcodes'} className="nav-link">Barcodes</Link></li>
           </ul>
           </nav>
           <hr />
           <Switch>
             <Route path='/plate' render={(props)=><Plate {...props} row='12' col='8'/>} />
+            <Route path='/plate384' render={(props)=><Plate {...props} row='24' col='16'/>} />
+            <Route path='/plate1536' render={(props)=><Plate {...props} row='64' col='24'/>} />
             <Route path='/barcodes' render={()=><Barcodes/>} />
           </Switch>
         </div>
       </Router>
     );
+  }
+}
+
+export const tableSize = () => {
+  var size = document.getElementById('tablesize')
+  if (size.value == '96') {
+    row = 12
+    col = 8
+  }
+  else if (size.value = '384') {
+    row = 24
+    col = 16
+  }
+  else if (size.value = '1536') {
+    row = 64
+    col = 24
   }
 }
 
