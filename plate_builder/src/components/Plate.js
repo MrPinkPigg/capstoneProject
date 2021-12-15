8430ab95581e9c6ce25240e83728e0e0530c6e63
mrpinkpigg <aaronknestaut@gmail.com>
2021-12-15 15:33:02 -0500

    exports work

--- plate_builder/src/components/Plate.js
+++ plate_builder/src/components/Plate.js
@@ -199,4 +199,16 @@ newTable();
 function exportJSON() {
     console.log(exportArr);
     sessionStorage.setItem("jsonData", exportArr);
+
+    downloadObjectAsJson(exportArr, "Exported_Data");
 }
+
+function downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
