2e7d770afe1283baccf43b8b0e0aa79a8661f489
KevinKoudelka <skkovi14@gmail.com>
2022-05-19 15:35:21 -0400

    added back button

--- plate_builder/src/components/Login.js
+++ plate_builder/src/components/Login.js
@@ -16,6 +16,7 @@ const Login = () =>{
         </div>
         <div class="mod hide" id='createAcc'>
             <div class="modal-content">
+                <h2 onClick={showLogin}>&#8592;</h2>
                 <h2>Create Account</h2>
                 <input class='inputs' placeholder='Username'></input> <br></br>
                 <input class='inputs' placeholder='Password'></input> <br></br>
@@ -23,7 +24,7 @@ const Login = () =>{
             </div>
         </div>
         <div class="mod hide" id='forgotPass'>
-            <div class="modal-content">
+            <div class="modal-content"> 
                 <h2>Reset Password</h2>
                 <body>Enter your email to receive password instructions</body>
                 <input class='inputs' placeholder='email'></input> <br></br>
@@ -60,6 +61,22 @@ const showForgot = () => {
     else {
         forgotScreen.classList.add("hide")
         signinScreen.classList.remove("hide")
+        createaccScreen.classList.add("hide")
+    }
+}
+
+const showLogin = () => {
+    const signinScreen = document.getElementById("signIn")
+    const createaccScreen = document.getElementById("createAcc")
+    const forgotScreen = document.getElementById("forgotPass")
+    if (signinScreen.classList.contains("hide")) {
+        signinScreen.classList.remove("hide")
+        forgotScreen.classList.add("hide")
+        createaccScreen.classList.add("hide")
+    }
+    else {
+        signinScreen.classList.add("hide")
+        forgotScreen.classList.remove("hide")
         createaccScreen.classList.add("hide")
     }
 }
